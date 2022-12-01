11c85fc622eb9e7a074df1937b7616cb1ccdf071
Moiseng <nsingi-moise@hotmail.com>
2022-12-01 16:12:31 +0100

    FEAT: add competition level's enum

---
+++ lib/enum/competition_level.dart
@@ -0,0 +1,12 @@
+
+enum CompetitionLevel {
+  amateur("amateur"),
+  club1("club1"),
+  club2("club2"),
+  club3("club3"),
+  club4("club4");
+
+  const CompetitionLevel(this.value);
+
+  final String value;
+}
